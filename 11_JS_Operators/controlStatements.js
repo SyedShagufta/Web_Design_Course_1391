e639882eecba42ba4bf622fc719f2c364cf51746
SyedShagufta <sweetyvox123@gmail.com>
2025-10-11 18:53:01 +0530

    JS Control Statements

---
+++ 11_JS_Operators/controlStatements.js
@@ -0,0 +1,46 @@
+var day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("Monday");
+    break;
+  case "Tuesday":
+    console.log("Tuesday");
+    break;
+  case "Wednesday":
+    console.log("Wednesday");
+    break;
+  case "Thursday":
+    console.log("Thursday");
+    break;
+  case "Friday":
+    console.log("Friday");
+    break;
+  case "Saturday":
+    console.log("Saturday");
+    break;
+  case "Sunday":
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+
+// while loop
+var a = 1;
+while (a < 11) {
+  console.log(a);
+  a++;
+}
+
+// do-while loop
+var a = 18;
+do {
+  console.log(a);
+  a--;
+} while (a > 0);
+
+// for loop
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
