92e6169da6f4d96dd4a504943e708fe96f48a6a9
SyedShagufta <sweetyvox123@gmail.com>
2025-10-11 18:52:16 +0530

    JS Basics

---
+++ 10_JS_Basics/script.js
@@ -0,0 +1,23 @@
+console.log("heyyyy");
+
+// redefining the variable
+
+var num = 20;
+console.log(num);
+
+num = 30;
+console.log(num);
+
+// redeclaring the variable
+
+var num = "Hey world";
+console.log(num);
+
+// we cannot redeclare block scope variables but can redefine
+let n1 = 10;
+console.log(n1);
+
+n1 = 1000;
+console.log(n1);
+
+// let n1 = "hey";
