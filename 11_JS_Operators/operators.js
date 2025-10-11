3900e2b2ce7672f7959d3023822b8b2116b4e36b
SyedShagufta <sweetyvox123@gmail.com>
2025-10-11 18:52:47 +0530

    JS Operators

---
+++ 11_JS_Operators/operators.js
@@ -0,0 +1,16 @@
+var num = 25;
+
+console.log(num++); //25
+console.log(num++); //26
+console.log(num++); // 27
+
+let a = 15;
+let b = 3;
+let c = -4;
+
+// 15 + 5 - 4 + 16
+const res = a++ - --c - ++b + a--;
+console.log(res);
+
+var myName = "Shagufta";
+console.log(`Hello, My name is ${myName}`);
